c21dbe76f461c89011cb14d82485e72aebeb970c
Imanul Aufa <imanulaufa@gmail.com>
2025-06-05 02:08:31 +0700

    feat(App.js): add component skills

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Hero from './components/Hero';
+import AboutMe from './components/AboutMe';
+import Skills from './components/Skills';
 // import './App.css'
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
   return (
     <main className='bg-gray-950'>
       <Hero />
+      <AboutMe />
+      <Skills />
     </main>
   )
 
